8443f26044e72bae4ea03e805b6fddaffb38df8c
Quoticus <38863921+Quoticus@users.noreply.github.com>
2018-07-26 17:57:41 -0300

    Started Tutorial 10
    Partway through Tutorial 10. Just barely began.

--- src/gameEngine/Main.java
+++ src/gameEngine/Main.java
@@ -5,12 +5,15 @@
  */
 package gameEngine;
 
+import entities.Camera;
+import entities.Entity;
 import renderEngine.Loader;
 import renderEngine.Renderer;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 import shaders.StaticShader;
 import textures.ModelTexture;
 
@@ -26,9 +29,9 @@ public class Main {
     public static void main(String[] args) {
         DisplayManager.createDisplay();
         Loader loader = new Loader();
-        Renderer renderer = new Renderer();
         StaticShader shader = new StaticShader();
-        
+        Renderer renderer = new Renderer(shader);
+        /*
         float[] vertices = {
             -0.5f, 0.5f, 0f, //V0
             -0.5f, -0.5f, 0f, //V1
@@ -41,15 +44,109 @@ public class Main {
                 3, 1, 2  //Bottom right triangle (V3, V1, V2)
         };
         
-        RawModel model = loader.loadToVAO(vertices, indices);
+        float[] textureCoords = {
+            0, 0, //V0
+            0, 1, //V1
+            1, 1, //V2
+            1, 0  //V3
+        };*/
+        
+        float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
+		};
+		
+		float[] textureCoords = {
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
+		};
+        
+        
+        
+        
+        
+        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
         ModelTexture texture = new ModelTexture(loader.loadTexture("texture"));
-        TexturedModel texturedModel = new TexturedModel(model, texture);
+        TexturedModel staticModel = new TexturedModel(model, texture);
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0, 0, 0, 1);
+        Camera camera = new Camera();
         
         while(!Display.isCloseRequested()){
-            //game logic
+            entity.increaseRotation(1, 1, 0);
+            camera.move();
             renderer.prepare();
             shader.start();
-            renderer.render(texturedModel);
+            shader.loadViewMatrix(camera);
+            renderer.render(entity, shader);
             shader.stop();
             DisplayManager.updateDisplay();
         }
