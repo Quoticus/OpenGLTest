736ba87b5d0d958776e7829047bd0fa2fb6c9448
Quoticus <38863921+Quoticus@users.noreply.github.com>
2018-08-03 16:51:52 -0300

    Completed Tutorial 14
    Completed Tutorial 14, creating and texturing a terrain.

--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -14,7 +14,7 @@ import org.lwjgl.util.vector.Vector3f;
  */
 public class Camera {
     
-    private Vector3f position = new Vector3f(0, 0, 0);
+    private Vector3f position = new Vector3f(0, 10, 100);
     private float pitch;
     private float yaw;
     private float roll;
@@ -25,13 +25,13 @@ public class Camera {
     
     public void move(){
         if(Keyboard.isKeyDown(Keyboard.KEY_W)){
-            position.z-=0.02f;
+            position.z-=2.02f;
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_D)){
-            position.x+=0.02f;
+            position.x+=2.02f;
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_A)){
-            position.x-=0.02f;
+            position.x-=2.02f;
         }
     }
 
--- src/gameEngine/Main.java
+++ src/gameEngine/Main.java
@@ -9,14 +9,15 @@ import entities.Camera;
 import entities.Entity;
 import entities.Light;
 import renderEngine.Loader;
-import renderEngine.Renderer;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
+import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
 import shaders.StaticShader;
+import terrains.Terrain;
 import textures.ModelTexture;
 
 /**
@@ -31,9 +32,6 @@ public class Main {
     public static void main(String[] args) {
         DisplayManager.createDisplay();
         Loader loader = new Loader();
-        StaticShader shader = new StaticShader();
-        Renderer renderer = new Renderer(shader);
-        
         RawModel model = OBJLoader.loadObjModel("dragon", loader);
         TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("whiteTexture")));
         ModelTexture texture = staticModel.getTexture();
@@ -41,21 +39,25 @@ public class Main {
         texture.setReflectivity(5);
         
         Entity entity = new Entity(staticModel, new Vector3f(0,-5,-25),0, 0, 0, 1);
-        Light light = new Light(new Vector3f(0,0,-17), new Vector3f(1, 1, 1));
+        Light light = new Light(new Vector3f(0,0,-20), new Vector3f(1, 1, 1));
+        
+        Terrain terrain = new Terrain(0, -1, loader, new ModelTexture(loader.loadTexture("texture")));
+        Terrain terrain2 = new Terrain(2, -1, loader, new ModelTexture(loader.loadTexture("texture")));
+        
         Camera camera = new Camera();
+        
+        MasterRenderer renderer = new MasterRenderer();
         
         while(!Display.isCloseRequested()){
             entity.increaseRotation(0, 0.5f, 0);
             camera.move();
-            renderer.prepare();
-            shader.start();
-            shader.loadLight(light);
-            shader.loadViewMatrix(camera);
-            renderer.render(entity, shader);
-            shader.stop();
+            renderer.processTerrain(terrain);
+            renderer.processTerrain(terrain2);
+            renderer.processEntity(entity);
+            renderer.render(light,camera);
             DisplayManager.updateDisplay();
         }
-        shader.cleanUp();
+        renderer.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
     }
